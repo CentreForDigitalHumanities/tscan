37c867b4d0e3911735227d3ea3918e8d77017030
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2012-10-30 10:53:27 +0000

    lot of refactoring. dropped Servermode for now
    git-svn-id: https://ilk.uvt.nl/svn/sources/tscan/trunk@15357 12f355fe-0486-481a-ad91-c297ab22b4e3

--- include/tscan/TscanServer.h
+++ include/tscan/TscanServer.h
@@ -30,24 +30,19 @@
 #include "ticcutils/LogStream.h"
 #include "tscan/Configuration.h"
 
-class TscanServerClass {
+class TscanClass {
  public:
-  TscanServerClass( Timbl::TimblOpts& opts );
-  ~TscanServerClass();
-  void Start( Timbl::TimblOpts& Opts );
+  TscanClass( Timbl::TimblOpts& opts );
+  ~TscanClass();
+  void init( Timbl::TimblOpts& Opts );
   void exec( const std::string&, std::ostream& );
   TiCC::LogStream cur_log;
  private:
-  bool RunServer();
-  bool RunOnce( const std::string& );
   bool getConfig( const std::string& );
   folia::Document *getFrogResult( std::istream& );
   std::string configFile;
-  std::string pidFile;
   std::string logFile;
   Configuration config;
-  bool doDaemon;
-  int tcp_socket;
   LogLevel dbLevel;
 };
 
--- src/tscan.cxx
+++ src/tscan.cxx
@@ -36,7 +36,7 @@
 #include "libfolia/folia.h"
 #include "libfolia/document.h"
 #include "ticcutils/StringOps.h"
-#include "tscan/TscanServer.h"
+#include "tscan/Tscan.h"
 #include "tscan/Alpino.h"
 #include "tscan/decomp.h"
 
@@ -44,10 +44,10 @@ using namespace std;
 using namespace TiCC;
 using namespace folia;
 
-#define SLOG (*Log(cur_log))
-#define SDBG (*Dbg(cur_log))
+TiCC::LogStream cur_log( "T-scan", StampMessage );
 
-#define LOG cerr
+#define LOG (*Log(cur_log))
+#define DBG (*Dbg(cur_log))
 
 const double NA = -123456789;
 
@@ -255,8 +255,7 @@ inline void usage(){
   cerr << "usage:  tscan" << endl;
 }
 
-TscanServerClass::TscanServerClass( Timbl::TimblOpts& opts ):
-  cur_log("T-Scan", StampMessage ){
+TscanClass::TscanClass( Timbl::TimblOpts& opts ){
   string val;
   bool mood;
   if ( opts.Find( "config", val, mood ) ){
@@ -277,84 +276,28 @@ TscanServerClass::TscanServerClass( Timbl::TimblOpts& opts ):
     exit( EXIT_FAILURE );
   }
   if ( opts.Find( 'D', val, mood ) ){
-    if ( val == "LogNormal" )
+    if ( val == "Normal" )
       cur_log.setlevel( LogNormal );
-    else if ( val == "LogDebug" )
+    else if ( val == "Debug" )
       cur_log.setlevel( LogDebug );
-    else if ( val == "LogHeavy" )
+    else if ( val == "Heavy" )
       cur_log.setlevel( LogHeavy );
-    else if ( val == "LogExtreme" )
+    else if ( val == "Extreme" )
       cur_log.setlevel( LogExtreme );
     else {
       cerr << "Unknown Debug mode! (-D " << val << ")" << endl;
     }
     opts.Delete( 'D' );
   }
-  string fileName;
-  if ( !opts.Find( "t", val, mood ) ){
-    cerr << "TScan server " << VERSION << endl;
-    cerr << "based on " << TimblServer::VersionName() << endl;
-    doDaemon = true;
-    dbLevel = LogNormal;
-    if ( opts.Find( "pidfile", val ) ) {
-      pidFile = val;
-      opts.Delete( "pidfile" );
-    }
-    if ( opts.Find( "logfile", val ) ) {
-      logFile = val;
-      opts.Delete( "logfile" );
-    }
-    if ( opts.Find( "daemonize", val ) ) {
-      doDaemon = ( val != "no" && val != "NO" && val != "false" && val != "FALSE" );
-      opts.Delete( "daemonize" );
-    }
-    RunServer();
+  if ( opts.Find( 't', val, mood ) ){
+    exec( val, cout );
   }
   else {
-    fileName = val;
-    cerr << "TScan " << VERSION << endl;
-    RunOnce( fileName );
+    cerr << "missing input file (-t option) " << endl;
   }
 }
 
-TscanServerClass::~TscanServerClass(){
-}
-
-struct childArgs{
-  TscanServerClass *Mother;
-  Sockets::ServerSocket *socket;
-  int maxC;
-};
-
-inline void Split( const string& line, string& com, string& rest ){
-  string::size_type b_it = line.find( '=' );
-  if ( b_it != string::npos ){
-    com = trim( line.substr( 0, b_it ) );
-    rest = trim( line.substr( b_it+1 ) );
-  }
-  else {
-    rest.clear();
-    com = line;
-  }
-}  
-
-void StopServerFun( int Signal ){
-  if ( Signal == SIGINT ){
-    exit(EXIT_FAILURE);
-  }
-  signal( SIGINT, StopServerFun );
-}  
-
-void BrokenPipeChildFun( int Signal ){
-  if ( Signal == SIGPIPE ){
-    signal( SIGPIPE, BrokenPipeChildFun );
-  }
-}
-
-void AfterDaemonFun( int Signal ){
-  if ( Signal == SIGCHLD ){
-    exit(1);
-  }
+TscanClass::~TscanClass(){
 }
 
 template <class M>
@@ -1547,19 +1490,19 @@ void docStats::print( ostream& os ) const {
   structStats::print( os );
 }
 
-Document *TscanServerClass::getFrogResult( istream& is ){
+Document *TscanClass::getFrogResult( istream& is ){
   string host = config.lookUp( "host", "frog" );
   string port = config.lookUp( "port", "frog" );
   Sockets::ClientSocket client;
   if ( !client.connect( host, port ) ){
-    SLOG << "failed to open Frog connection: "<< host << ":" << port << endl;
-    SLOG << "Reason: " << client.getMessage() << endl;
+    LOG << "failed to open Frog connection: "<< host << ":" << port << endl;
+    LOG << "Reason: " << client.getMessage() << endl;
     return 0;
   }
-  SDBG << "start input loop" << endl;
+  DBG << "start input loop" << endl;
   string line;
   while ( getline( is, line ) ){
-    SDBG << "read: " << line << endl;
+    DBG << "read: " << line << endl;
     client.write( line + "\n" );
   }
   client.write( "\nEOT\n" );
@@ -1570,25 +1513,26 @@ Document *TscanServerClass::getFrogResult( istream& is ){
       break;
     result += s + "\n";
   }
-  SDBG << "received data [" << result << "]" << endl;
+  DBG << "received data [" << result << "]" << endl;
   Document *doc = 0;
   if ( !result.empty() && result.size() > 10 ){
-    SDBG << "start FoLiA parsing" << endl;
+    DBG << "start FoLiA parsing" << endl;
     doc = new Document();
     try {
       doc->readFromString( result );
-      SDBG << "finished" << endl;
+      DBG << "finished" << endl;
     }
     catch ( std::exception& e ){
-      SLOG << "FoLiaParsing failed:" << endl
+      LOG << "FoLiaParsing failed:" << endl
 	   << e.what() << endl;	  
     }
   }
   return doc;
 }
 
-void TscanServerClass::exec( const string& file, ostream& os ){
-  SLOG << "try file ='" << file << "'" << endl;
+void TscanClass::exec( const string& file, ostream& os ){
+  cerr << "TScan " << VERSION << endl;
+  LOG << "try file ='" << file << "'" << endl;
   ifstream is( file.c_str() );
   if ( !is ){
     os << "failed to open file '" << file << "'" << endl;
@@ -1608,197 +1552,6 @@ void TscanServerClass::exec( const string& file, ostream& os ){
     }
   }
 }
-
-// ***** This is the routine that is executed from a new thread **********
-void *runChild( void *arg ){
-  childArgs *args = (childArgs *)arg;
-  TscanServerClass *theServer = args->Mother;
-  LogStream *cur_log = &theServer->cur_log;
-  Sockets::Socket *Sock = args->socket;
-  static int service_count = 0;
-  static pthread_mutex_t my_lock = PTHREAD_MUTEX_INITIALIZER;
-  //
-  // use a mutex to update the global service counter
-  //
-  pthread_mutex_lock( &my_lock );
-  service_count++;
-  int nw = 0;
-  if ( service_count > args->maxC ){
-    Sock->write( "Maximum connections exceeded\n" );
-    Sock->write( "try again later...\n" );
-    pthread_mutex_unlock( &my_lock );
-    SLOG << "Thread " << pthread_self() << " refused " << endl;
-  }
-  else {
-    // Greeting message for the client
-    //
-    pthread_mutex_unlock( &my_lock );
-    time_t timebefore, timeafter;
-    time( &timebefore );
-    // report connection to the server terminal
-    //
-    SLOG << "Thread " << pthread_self() << ", Socket number = "
-	 << Sock->getSockId() << ", started at: " 
-	 << asctime( localtime( &timebefore) );
-    signal( SIGPIPE, BrokenPipeChildFun );
-    fdistream is( Sock->getSockId() );
-    fdostream os( Sock->getSockId() );
-    os << "Welcome to the T-scan server." << endl;
-    string line;
-    if ( getline( is, line ) ){
-      line = TiCC::trim( line );
-      SDBG << "FirstLine='" << line << "'" << endl;
-      theServer->exec( line, os );
-    }
-    time( &timeafter );
-    SLOG << "Thread " << pthread_self() << ", terminated at: " 
-	 << asctime( localtime( &timeafter ) );
-    SLOG << "Total time used in this thread: " << timeafter - timebefore 
-	 << " sec, " << nw << " words processed " << endl;
-  }
-  // exit this thread
-  //
-  pthread_mutex_lock( &my_lock );
-  service_count--;
-  SLOG << "Socket total = " << service_count << endl;
-  pthread_mutex_unlock( &my_lock );
-  delete Sock;
-  return 0;
-}
-
-bool TscanServerClass::RunOnce( const string& file ){
-  if ( !logFile.empty() ){
-    ostream *tmp = new ofstream( logFile.c_str() );
-    if ( tmp && tmp->good() ){
-      cerr << "switching logging to file " << logFile << endl;
-      cur_log.associate( *tmp );
-      cur_log.message( "T-Scan:" );
-      SLOG << "Started logging " << endl;	
-    }
-    else {
-      cerr << "unable to create logfile: " << logFile << endl;
-      exit(EXIT_FAILURE);
-    }
-  }
-  exec( file, cout );
-  return true;
-}
-  
-bool TscanServerClass::RunServer(){
-  int maxConn = 25;
-  int serverPort = -1;
-  string value;
-  value = config.lookUp( "maxconn" );
-  if ( !value.empty() && !Timbl::stringTo( value, maxConn ) ){
-    cerr << "invalid value for maxconn" << endl;
-    return false;
-  }
-  value = config.lookUp( "port" );
-  if ( !value.empty() && !Timbl::stringTo( value, serverPort ) ){
-    cerr << "invalid value for port" << endl;
-    return false;
-  }
-
-  cerr << "trying to start a Server on port: " << serverPort << endl
-       << "maximum # of simultaneous connections: " << maxConn
-       << endl;
-  if ( !logFile.empty() ){
-    ostream *tmp = new ofstream( logFile.c_str() );
-    if ( tmp && tmp->good() ){
-      cerr << "switching logging to file " << logFile << endl;
-      cur_log.associate( *tmp );
-      cur_log.message( "T-Scan:" );
-      SLOG << "Started logging " << endl;	
-    }
-    else {
-      cerr << "unable to create logfile: " << logFile << endl;
-      cerr << "not started" << endl;
-      exit(EXIT_FAILURE);
-    }
-  }
-  int start = 0;
-  if ( doDaemon ){
-    signal( SIGCHLD, AfterDaemonFun );
-    if ( logFile.empty() )
-      start = TimblServer::daemonize( 1, 1 );
-    else
-      start = TimblServer::daemonize( 0, 0 );
-  }
-  if ( start < 0 ){
-    LOG << "Failed to daemonize error= " << strerror(errno) << endl;
-    exit(EXIT_FAILURE);
-  };
-  if ( !pidFile.empty() ){
-    // we have a liftoff!
-    // signal it to the world
-    remove( pidFile.c_str() ) ;
-    ofstream pid_file( pidFile.c_str() ) ;
-    if ( !pid_file ){
-      LOG << "Unable to create pidfile:"<< pidFile << endl;
-      LOG << "TimblServer NOT Started" << endl;
-      exit(EXIT_FAILURE);
-    }
-    else {
-      pid_t pid = getpid();
-      pid_file << pid << endl;
-    }
-  }
-  
-  // set the attributes
-  pthread_attr_t attr;
-  if ( pthread_attr_init(&attr) ||
-       pthread_attr_setdetachstate( &attr, PTHREAD_CREATE_DETACHED ) ){
-    LOG << "Threads: couldn't set attributes" << endl;
-    exit(EXIT_FAILURE);
-  }
-  //
-  // setup Signal handling to abort the server.
-  signal( SIGINT, StopServerFun );
-  
-  pthread_t chld_thr;
-  
-  // start up server
-  // 
-  LOG << "Started Server on port: " << serverPort << endl
-      << "Maximum # of simultaneous connections: " << maxConn
-      << endl;
-  
-  Sockets::ServerSocket server;
-  string portString = Timbl::toString<int>(serverPort);
-  if ( !server.connect( portString ) ){
-    LOG << "Failed to start Server: " << server.getMessage() << endl;
-    exit(EXIT_FAILURE);
-  }
-  if ( !server.listen( maxConn ) < 0 ){
-    LOG << "Server: listen failed " << strerror( errno ) << endl;
-    exit(EXIT_FAILURE);
-  };
-  
-  while(true){ // waiting for connections loop
-    Sockets::ServerSocket *newSock = new Sockets::ServerSocket();
-    if ( !server.accept( *newSock ) ){
-      if( errno == EINTR )
-	continue;
-      else {
-	LOG << "Server: Accept Error: " << server.getMessage() << endl;
-	exit(EXIT_FAILURE);
-      }
-    }
-    LOG << "Accepting Connection " << newSock->getSockId()
-	<< " from remote host: " << newSock->getClientName() << endl;
-    
-    // create a new thread to process the incoming request 
-    // (The thread will terminate itself when done processing
-    // and release its socket handle)
-    //
-    childArgs *args = new childArgs();
-    args->Mother = this;
-    args->maxC = maxConn;
-    args->socket = newSock;
-    pthread_create( &chld_thr, &attr, runChild, (void *)args );
-    // the server is now free to accept another socket request 
-  }
-}
   
 int main(int argc, char *argv[]) {
   Timbl::TimblOpts opts( argc, argv );
@@ -1813,7 +1566,7 @@ int main(int argc, char *argv[]) {
        opts.Find( "version", val, mood ) ){
     exit( EXIT_SUCCESS );
   }
-  TscanServerClass server( opts );
+  TscanClass tscan( opts );
   exit(EXIT_SUCCESS);
 }
 
