49ae13422495d82c01ddd78882a627af42d34b13
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2012-10-31 11:22:59 +0000

    start to add tests
    git-svn-id: https://ilk.uvt.nl/svn/sources/tscan/trunk@15372 12f355fe-0486-481a-ad91-c297ab22b4e3

---
+++ tests/testone.sh
@@ -0,0 +1,34 @@
+# /bin/sh
+
+if [ "$tscan_bin" = "" ];
+then echo "tscan_bin not set";
+     exit;
+fi
+
+OK="\033[1;32m OK  \033[0m"
+FAIL="\033[1;31m  FAILED  \033[0m"
+KNOWNFAIL="\033[1;33m  KNOWN FAILURES  \033[0m"
+
+export comm="$VG $tscan_bin/tscan"
+
+for file in $1
+do if test -e $file
+    then 
+	\rm -f $file.diff
+	\rm -f $file.err
+	\rm -f $file.out
+	echo -n "Tscanning  $file "
+	$comm -t $file > $file.out 2> $file.err
+	xmldiff /tmp/folia.2.xml $file.ok >& $file.diff
+	if [ $? -ne 0 ];
+	then 
+	    echo -e $FAIL;	
+	    echo "differences logged in $file.diff";
+	else
+	  echo -e $OK
+	  rm $file.diff
+	fi
+    else
+	echo "file $file not found"
+    fi
+done
