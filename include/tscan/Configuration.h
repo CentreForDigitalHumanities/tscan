9c57ad95f1d32033aa2fbd1ff9b10731ae8399c1
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2012-11-21 15:59:07 +0000

    oespjes
    git-svn-id: https://ilk.uvt.nl/svn/sources/tscan/trunk@15451 12f355fe-0486-481a-ad91-c297ab22b4e3

--- include/tscan/Configuration.h
+++ include/tscan/Configuration.h
@@ -5,14 +5,14 @@
   Copyright (c) 2006 - 2012
   Tilburg University
 
-  This file is part of frog.
+  This file is part of tscan.
 
-  frog is free software; you can redistribute it and/or modify
+  tscan is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 3 of the License, or
   (at your option) any later version.
 
-  frog is distributed in the hope that it will be useful,
+  tscan is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
   GNU General Public License for more details.
--- src/Configuration.cxx
+++ src/Configuration.cxx
@@ -4,14 +4,14 @@
   Copyright (c) 2006 - 2012
   Tilburg University
 
-  This file is part of frog.
+  This file is part of tscan.
 
-  frog is free software; you can redistribute it and/or modify
+  tscan is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 3 of the License, or
   (at your option) any later version.
 
-  frog is distributed in the hope that it will be useful,
+  tscan is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
   GNU General Public License for more details.
@@ -31,7 +31,7 @@
 #include <stdexcept>
 #include <iostream>
 #include "ticcutils/StringOps.h"
-#include "frog/Configuration.h"
+#include "tscan/Configuration.h"
 
 
 using namespace std;
