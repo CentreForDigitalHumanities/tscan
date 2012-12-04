3f57c245095e0023bdaee65b8442273b518f6529
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2012-12-04 15:47:01 +0000

    updated paths
    git-svn-id: https://ilk.uvt.nl/svn/sources/tscan/trunk@15506 12f355fe-0486-481a-ad91-c297ab22b4e3

--- decompound.sh
+++ decompound.sh
@@ -1,6 +1,13 @@
 # /bin/sh
-export LD_LIBRARY_PATH=/home/sloot/svn/decompounder
 
-cd /home/sloot/svn/decompounder
+
+
+if [[ `hostname` == "applejack" ]]; then
+	export LD_LIBRARY_PATH="/scratch2/www/webservices-lst/live/repo/tscan/decompounder:$LD_LIBRARY_PATH"
+        cd /scratch2/www/webservices-lst/live/repo/tscan/decompounder
+else
+	export LD_LIBRARY_PATH="/home/sloot/svn/decompounder:$LD_LIBRARY_PATH"
+	cd /home/sloot/svn/decompounder
+fi
 
 ./PolderDecompounder <$1 > $2
--- surprise.sh
+++ surprise.sh
@@ -1,6 +1,10 @@
 # /bin/sh
 
-cd /home/sloot/HumDep3.0/Code
+if [[ `hostname` == "applejack" ]]; then
+	cd /scratch2/www/webservices-lst/live/repo/tscan/HumDep3.0/Code
+else
+	cd /home/sloot/HumDep3.0/Code
+fi
 
 ./depParse -train Dutch -file $1
 
