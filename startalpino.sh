4ee15a6319403a8bc32af3985b29eea657415ac4
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2012-12-18 09:38:53 +0000

    added example startup script for the Alpino parser
    git-svn-id: https://ilk.uvt.nl/svn/sources/tscan/trunk@15537 12f355fe-0486-481a-ad91-c297ab22b4e3

---
+++ startalpino.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+export PORT=6666
+export TIMEOUT=20000
+export MEMLIMIT=1000M
+export TMPDIR=/tmp
+
+## this one is for parsing
+
+PROLOGMAXSIZE=${MEMLIMIT} Alpino -notk -fast user_max=${TIMEOUT}\
+ server_kind=parse\
+ server_port=${PORT}\
+ assume_input_is_tokenized=on\
+ debug=1\
+ end_hook=xml\
+-init_dict_p\
+ batch_command=alpino_server 2> ${TMPDIR}/alpino_server.log &
+
+echo "Alpino started"
