c3c05e6cf86664fc09648682130259aeaf743c1a
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2013-02-08 20:46:50 +0000

    adapted for non-Ko
    git-svn-id: https://ilk.uvt.nl/svn/sources/tscan/trunk@15655 12f355fe-0486-481a-ad91-c297ab22b4e3

--- startwopr.sh
+++ startwopr.sh
@@ -1,7 +1,11 @@
-#! /bin/sh
-PATH=/home/sloot/usr/local/
+#! /bin/bash
+if [ `hostname` == "applejack" ]; then
+	WOPRPATH="/vol/customopt/uvt-ru/"
+else
+	WOPRPATH="/home/sloot/usr/local/"
+fi
 PORT=6345
 DATADIR=./data
 
-${PATH}/bin/wopr -r xmlserver -p ibasefile:${DATADIR}/sonar_newspapercorp_tokenized.3.txt.l2r0_-a4+D.ibase,lexicon:${DATADIR}/sonar_newspapercorp_tokenized.3.txt.lex,keep:1,mode:1,verbose:2,port:${PORT}
+${WOPRPATH}/bin/wopr -r xmlserver -p ibasefile:${DATADIR}/sonar_newspapercorp_tokenized.3.txt.l2r0_-a4+D.ibase,lexicon:${DATADIR}/sonar_newspapercorp_tokenized.3.txt.lex,keep:1,mode:1,verbose:2,port:${PORT}
 
