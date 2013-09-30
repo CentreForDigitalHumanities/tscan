f10ff3644deee413f70319ba4aaa19eb9602591b
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2013-09-30 08:53:11 +0000

    log
    git-svn-id: https://ilk.uvt.nl/svn/sources/tscan/trunk@16537 12f355fe-0486-481a-ad91-c297ab22b4e3

--- webservice/startfrog.sh
+++ webservice/startfrog.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 if [ `hostname` == "applejack" ]; then
 	FROGPATH="/vol/customopt/uvt-ru/"
+    LOGFILE= "/scratch2/www/webservices-lst/live/writable/tscan/frog.log"
 else
 	FROGPATH="/home/sloot/usr/local/"
+    LOGFILE="/tmp/frog-tscan.log"
 fi
 PORT=7345
 ID=tscan
 CONFIG=${FROGPATH}/etc/frog/tscan-frog.cfg
 
-${FROGPATH}/bin/frog -X --id=${ID} -c ${CONFIG} --skip=mp -S${PORT}
+${FROGPATH}/bin/frog -X --id=${ID} -c ${CONFIG} --skip=mp -S${PORT} 2> $LOGFILE
--- webservice/startwopr.sh
+++ webservice/startwopr.sh
@@ -5,7 +5,7 @@ else
 	WOPRPATH="/home/sloot/usr/local/"
 fi
 PORT=6345
-DATADIR=./data
+DATADIR=../data
 
 ${WOPRPATH}/bin/wopr -r xmlserver -p ibasefile:${DATADIR}/sonar_newspapercorp_tokenized.3.txt.l2r0_-a4+D.ibase,lexicon:${DATADIR}/sonar_newspapercorp_tokenized.3.txt.lex,keep:1,mode:1,verbose:0,port:${PORT}
 
