4533d423fdb9cc9e01125eb22a4c457d921ff54d
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2013-11-04 13:18:22 +0000

    attempt to get it to work
    git-svn-id: https://ilk.uvt.nl/svn/sources/tscan/trunk@16668 12f355fe-0486-481a-ad91-c297ab22b4e3

---
+++ webservice/startlsa2.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+PORT=12346
+DATADIR=../data
+
+./gensim_lsa2_server.py ${PORT} ${DATADIR}/newspaper_contentlemma_dictionary_filtered.dict ${DATADIR}/lsa_newspaper_lemma_model_700_filtered.lsi
