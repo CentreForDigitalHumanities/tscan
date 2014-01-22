4d536c29fdb7776ed3b0deb5957d677fa062baf6
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2014-01-22 09:30:08 +0000

    'fix'
    git-svn-id: https://ilk.uvt.nl/svn/sources/tscan/trunk@16868 12f355fe-0486-481a-ad91-c297ab22b4e3

--- webservice/startfrog.sh
+++ webservice/startfrog.sh
@@ -3,12 +3,14 @@ if [ `hostname` == "applejack" ]; then
 	FROGPATH="/vol/customopt/uvt-ru/"
     LOGFILE="/scratch2/www/webservices-lst/live/writable/tscan/frog.log"
 else
-	FROGPATH="/home/sloot/usr/local/"
+    FROGPATH="/home/sloot/usr/local/"
     LOGFILE="/tmp/frog-tscan.log"
 fi
 PORT=7001
 ID=tscan
 CONFIG=${FROGPATH}/etc/frog/tscan-frog.cfg
+
+mv $LOGFILE $LOGFILE.sav # poor mans solution
 
 ${FROGPATH}/bin/frog -X --id=${ID} -c ${CONFIG} --skip=mp -S${PORT} 2> $LOGFILE
 
