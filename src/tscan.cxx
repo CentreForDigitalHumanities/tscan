da00045c25b98501c13a4ad0a9ea72d6f8dc563f
Maarten van Gompel <proycon@anaproy.nl>
2015-08-28 17:16:02 +0200

    license-update in sources

--- src/tscan.cxx
+++ src/tscan.cxx
@@ -1,27 +1,22 @@
 /*
-  $Id$
-  $URL$
+  T-scan
 
-  Copyright (c) 1998 - 2014
+  Copyright (c) 1998 - 2015
 
   This file is part of tscan
 
   tscan is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
+  it under the terms of the GNU Affero General Public License as published by
   the Free Software Foundation; either version 3 of the License, or
   (at your option) any later version.
 
   tscan is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  GNU Affere General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
+  You should have received a copy of the GNU Affero General Public License
   along with this program; if not, see <http://www.gnu.org/licenses/>.
-
-  For questions and suggestions, see:
-
-  or send mail to:
 
 */
 
