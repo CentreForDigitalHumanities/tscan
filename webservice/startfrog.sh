d29fe42bf3fc16b1bd255dc522458821f3cbc98e
Maarten van Gompel <proycon@anaproy.nl>
2016-03-18 10:30:17 +0100

    limit frog threads

--- webservice/startfrog.sh
+++ webservice/startfrog.sh
@@ -2,9 +2,11 @@
 if [ `hostname` == "applejack" ]; then
 	FROGPATH="/scratch2/www/lamachine"
     LOGFILE="/scratch2/www/webservices-lst/live/writable/tscan/frog.log"
+    THREADS=8
 else
     FROGPATH="/usr/local/"
     LOGFILE="/tmp/frog-tscan.log"
+    THREADS=0 #all we can get
 fi
 PORT=7001
 ID=tscan
@@ -12,4 +14,4 @@ CONFIG=${FROGPATH}/etc/frog/tscan-frog.cfg
 
 mv $LOGFILE $LOGFILE.sav # poor mans solution
 
-frog -X --id=${ID} -c ${CONFIG} --skip=mp -S${PORT} 2> $LOGFILE
+frog -X --id=${ID} -c ${CONFIG} --skip=mp --threads=${THREADS} -S${PORT} 2> $LOGFILE
