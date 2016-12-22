5956247538b5c6c2ca93da4f65676b483883dc99
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2016-12-22 16:23:31 +0100

    updated script

--- tests/testone.sh
+++ tests/testone.sh
@@ -15,23 +15,19 @@ for file in $@
 do if test -e $file
     then
 	\rm -f $file.diff
-	\rm -f $file.diff.fd
 	\rm -f $file.err
 	\rm -f $file.out
 	\rm -f $file.tscan.xml
 	echo -n "Tscanning  $file "
 	$comm --skip=c -t $file > $file.out 2> $file.err
-	./foliadiff.sh $file.tscan.xml $file.ok >& $file.diff.fd
+	./foliadiff.sh $file.tscan.xml $file.ok >& $file.diff
 	if [ $? -ne 0 ];
 	then
 	    echo -e $FAIL;
-	    diff -w --ignore-matching-lines=".?*-annotation .?*" --ignore-matching-lines=".*generator=.*" $file.tscan.xml $file.ok >& $file.diff
 	    echo "differences logged in $file.diff";
-	    echo "xmldiff logged in $file.diff.fd";
 	else
 	  echo -e $OK
-	  rm -f $file.diff
-	  rm $file.diff.fd
+	  rm $file.diff
 	fi
     else
 	echo "file $file not found"
