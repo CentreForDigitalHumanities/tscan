fde3e8d08b9198543694b188b7db33e2ae5c765b
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2016-12-22 16:23:14 +0100

    updated script

--- tests/foliadiff.sh
+++ tests/foliadiff.sh
@@ -28,10 +28,9 @@ t1=/tmp/${1##*/}.s1
 t2=/tmp/${2##*/}.s2
 $exe --strip "$1" > $t1
 $exe --strip "$2" > $t2
-diff -w $t1 $t2
+diff -w --ignore-matching-lines=".?*-annotation .?*" --ignore-matching-lines=".*generator=.*" $t1 $t2
 if [ $? -ne 0 ]
 then
-    echo "$1 $2 differ"
     exit 1
 fi
 
