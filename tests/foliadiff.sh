7bbcd105cbfe312ac23e788f897d3f7e9d52b329
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2016-12-22 12:06:07 +0100

    added a new script

---
+++ tests/foliadiff.sh
@@ -0,0 +1,38 @@
+# /bin/sh -x
+if [ "$exe" = "" ]
+then
+    exe="$HOME/usr/local/bin/folialint"
+    if [ ! -f $exe ]
+    then
+	exe="/exp/sloot/usr/local/bin/folialint"
+	if [ ! -f $exe ]
+	then
+	    exe="/usr/local/bin/folialint"
+	fi
+    fi
+fi
+
+if [ "$exe" = "" ];
+then
+    "cannot locate folialint"
+    exit 1
+fi
+
+if [ $# -ne 2 ]
+  then
+      echo "two arguments expected"
+      exit 1
+fi
+
+t1=/tmp/${1##*/}.s1
+t2=/tmp/${2##*/}.s2
+$exe --strip "$1" > $t1
+$exe --strip "$2" > $t2
+xmldiff $t1 $t2
+if [ $? -ne 0 ]
+then
+    echo "$1 $2 diff"
+    exit 1
+fi
+
+exit 0
