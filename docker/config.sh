452e3010bd9781417b7c7d34a117bbb1e1c675d5
Sheean Spoel <s.j.j.spoel@uu.nl>
2020-11-09 21:37:45 +0100

    set host_vars endpoints ->  updates lm_base_url!

--- docker/config.sh
+++ docker/config.sh
@@ -2,3 +2,6 @@
 source lamachine-activate
 lamachine-config force_https true
 lamachine-config hostname tst.tscan.hum.uu.nl
+
+# TODO: this is probably some bug or configuration issue in LaMachine?
+cp /usr/local/src/LaMachine/host_vars/develop.yml /usr/local/src/LaMachine/host_vars/localhost.yml
