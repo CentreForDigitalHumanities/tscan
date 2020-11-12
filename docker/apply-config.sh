f2a330f91ebcab776e3e803178207dbeda6f7b7d
Sheean Spoel <s.j.j.spoel@uu.nl>
2020-11-12 12:15:24 +0100

    derp

--- docker/apply-config.sh
+++ docker/apply-config.sh
@@ -9,7 +9,7 @@ fi
 # (to update the start webserver script and CLAM configuration)
 cd "/usr/local/src/LaMachine"
 echo "---" > "install.tmp.yml"
-echo "- hosts: develop" >> "install.tmp.yml"
+grep "hosts:" install.yml >> "install.tmp.yml"
 echo "  roles: [ lamachine-core ]"  >> "install.tmp.yml"
 
 ansible-playbook -i "hosts.ini" "install.tmp.yml" -v --extra-vars "ansible_python_interpreter='$(which python3)'"
