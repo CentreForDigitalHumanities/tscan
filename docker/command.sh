16640a8126f796fedcd45d6075e44ea735aace94
Sheean Spoel <s.j.j.spoel@uu.nl>
2020-11-12 20:32:55 +0100

    Make sure server is stopped before starting

--- docker/command.sh
+++ docker/command.sh
@@ -1,6 +1,8 @@
 source lamachine-activate
 # work around for https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=934731
 sudo chmod -f 600 /run/uwsgi-emperor.pid
+lamachine-stop-webserver
+sudo killall -q uwsgi
 
 # sudo for moving data-folder required
 sudo lamachine-start-webserver
