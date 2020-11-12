112bc849ab4c364e083b41dc8524354eaba9594a
Sheean Spoel <s.j.j.spoel@uu.nl>
2020-11-12 10:55:34 +0100

    Use lamachine-update fully, fix server restart

--- docker/build.sh
+++ docker/build.sh
@@ -2,16 +2,16 @@
 
 source lamachine-activate
 
-LAMACHINE_HOSTNAME=$(lamachine-config hostname | cut -c11-)
+# LAMACHINE_HOSTNAME=$(lamachine-config hostname | cut -c11-)
 
-echo "flaturl: https://$LAMACHINE_HOSTNAME/flat
-forcehttps: true" | sudo tee /usr/local/etc/clam_base.config.yml
+# echo "flaturl: https://$LAMACHINE_HOSTNAME/flat
+# forcehttps: true" | sudo tee /usr/local/etc/clam_base.config.yml
 
 sudo chown -R lamachine:lamachine /usr/local/src/tscan
 cp -R /deployment/roles/* /usr/local/src/LaMachine/roles
 
 lamachine-add tscan
-lamachine-update --only tscan
+lamachine-update
 
 # clean up
 rm -rf /usr/local/src/tscan/data
--- docker/command.sh
+++ docker/command.sh
@@ -1,4 +1,17 @@
 source lamachine-activate
+# work around for https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=934731
+sudo chmod -f 600 /run/uwsgi-emperor.pid
+
+# # call moving data manually if it has been configured
+# # (the lamachine-start-webserver script hasn't been updated
+# # to reflect that)
+# LAMACHINE_MOVE_SHARE_WWW_DATA=$(lamachine-config move_share_www_data | cut -c22-)
+# if [ "$LAMACHINE_MOVE_SHARE_WWW_DATA" = "true" ]; then
+#     /usr/local/bin/lamachine-move-www-data
+# else
+#     echo "www-data isn't going anywhere"
+# fi
+
 lamachine-start-webserver
 
 # keep it open
