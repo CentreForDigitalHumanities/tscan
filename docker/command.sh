5a5953cb86d500b633a6bbe1e2abbcfd1a133706
Sheean Spoel <s.j.j.spoel@uu.nl>
2020-11-12 16:00:37 +0100

    sudo

--- docker/command.sh
+++ docker/command.sh
@@ -2,7 +2,8 @@ source lamachine-activate
 # work around for https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=934731
 sudo chmod -f 600 /run/uwsgi-emperor.pid
 
-lamachine-start-webserver
+# sudo for moving data-folder required
+sudo lamachine-start-webserver
 
 # keep it open
 bash
