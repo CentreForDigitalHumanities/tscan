fbfde501a2d2545be371c6a7f1846b013a8ebdc9
Sheean Spoel <s.j.j.spoel@uu.nl>
2020-11-12 12:14:57 +0100

    WRONG LINE

--- docker/apply-config.sh
+++ docker/apply-config.sh
@@ -5,13 +5,13 @@ if [ ! -z "$PYTHONPATH" ]; then
     export PYTHONPATH=""
 fi
 
-echo "---
-- hosts: develop
-  roles: [ lamachine-core ]" > "install.tmp.yml"
-
 # Force updating the lamachine-core
 # (to update the start webserver script and CLAM configuration)
 cd "/usr/local/src/LaMachine"
+echo "---" > "install.tmp.yml"
+echo "- hosts: develop" >> "install.tmp.yml"
+echo "  roles: [ lamachine-core ]"  >> "install.tmp.yml"
+
 ansible-playbook -i "hosts.ini" "install.tmp.yml" -v --extra-vars "ansible_python_interpreter='$(which python3)'"
 
 if [ ! -z "$OLDPYTHONPATH" ]; then
