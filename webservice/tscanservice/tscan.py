6335026d62da87be94e323fb3c30a4b8928d2153
Sheean Spoel <s.j.j.spoel@uu.nl>
2023-09-03 21:31:14 +0200

    Updated default settings by @pandermaat
    Co-authored-by: Henk Pander Maat <h.l.w.pandermaat@uu.nl>

--- webservice/tscanservice/tscan.py
+++ webservice/tscanservice/tscan.py
@@ -374,23 +374,23 @@ parameters_list = [
     ChoiceParameter(id='alpinoOutput', name='Store Alpino output',
                     description='Store the Alpino output and input as a treebank file', choices=['yes', 'no'], default='no'),
     ChoiceParameter(id='useWopr', name='Use Wopr', description='Use Wopr?', choices=[
-        'yes', 'no'], default='yes'),
+        'yes', 'no'], default='no'),
     ChoiceParameter(id='sentencePerLine', name='One sentence per line',
                     description='Are the input texts already split per line?', choices=['yes', 'no'], default='no'),
     ChoiceParameter(id='prevalence', name='Prevalence data', description='Use prevalence data (http://crr.ugent.be/programs-data/word-prevalence-values) for',
                     choices=[('nl', 'The Netherlands'), ('be', 'Belgium')], default='nl'),
     ChoiceParameter(id='word_freq_lex', name='Word Frequency List', description="Word frequency list",
-                    choices=wordfreqlist, default="SoNaR500.wordfreqlist_words.freq"),
+                    choices=wordfreqlist, default="subtlex_words.freq"),
     ChoiceParameter(id='lemma_freq_lex', name='Lemma Frequency List', description="Lemma frequency list",
-                    choices=lemmafreqlist, default="SoNaR500.wordfreqlist_lemma.freq"),
+                    choices=lemmafreqlist, default="subtlex_lemma.freq"),
     ChoiceParameter(id='top_freq_lex', name='Top Frequency List', description="Top frequency list",
-                    choices=topfreqlist, default="SoNaR500.wordfreqlist20000.freq")
+                    choices=topfreqlist, default="subtlex_words20000.freq")
 ]
 
 if compound_methods != None:
     parameters_list.append(
         ChoiceParameter(id='compoundSplitterMethod', name='Compound split method', description='Method used by compound splitting module',
-                        choices=compound_methods, default='secos'))
+                        choices=compound_methods, default='never'))
 
 PARAMETERS = [
     ('Parameters', parameters_list)
