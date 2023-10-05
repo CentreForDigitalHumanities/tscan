1ae5af3f22929366b3d58a66dddb324153e76d2c
Sheean Spoel <s.j.j.spoel@uu.nl>
2023-10-05 14:17:41 +0200

    Remove old big Frog crash dumps

--- webservice/startfrog.sh
+++ webservice/startfrog.sh
@@ -10,5 +10,6 @@ ID=tscan
 mv $LOGFILE $LOGFILE.sav # poor mans solution
 cd $LOGDIR # make sure the debug log ends up in the log directory
 # Remove old logs
+rm -f core.*
 rm -f `ls -t *.debug | awk 'NR>4'`
 frog -X --deep-morph --id=${ID} --skip=mp -S"${PORT}" 2> $LOGFILE
