8579ad43614833d7309b4e229713be88b0e3c4b4
Sheean Spoel <s.j.j.spoel@uu.nl>
2025-01-24 09:35:07 +0100

    Create nginx log directory

--- docker/deployment/runit.d/nginx.run.sh
+++ docker/deployment/runit.d/nginx.run.sh
@@ -2,6 +2,7 @@
 
 # This script is meant to be invoked via runit (installed in /etc/service/nginx/run), not directly
 
+mkdir -p ${LOGDIR=/var/log}/nginx
 LOGFILE=${LOGDIR=/var/log}/nginx/access.log
 touch $LOGFILE
 nginx -g "daemon off; error_log $LOGFILE info;"
